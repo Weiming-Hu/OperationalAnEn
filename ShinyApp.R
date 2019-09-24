7c02e0b41c10057d0b22bd3b9a79b8d68fcf49ff
Ben Tate <taterzee@gmail.com>
2019-09-23 20:57:48 -0400

    These are the updates to the App for the meeting tomorrow. A lot of problems currently, however they appear to be easy to fix.

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,4 +1,4 @@
-for (library in c('shiny', 'leaflet', 'maps')) {
+for (library in c('shiny', 'leaflet', 'maps','raster','stringr','rgdal')) {
 	if (requireNamespace(library)) {
 		install.packages(library)
 	} else {
@@ -6,12 +6,32 @@ for (library in c('shiny', 'leaflet', 'maps')) {
 	}	
 }
 
+#Hardcoded the date here so it works. Can be changed by removing line 12 and changing 'date2' to 'Date'.
+date1 <- Sys.Date()
+date2 <- str_remove_all(date1,"-")
+Date <- "20190920"
+
+
+YM <- substr(Date, 1, nchar(Date)-2)
+
+url <- paste("https://nomads.ncdc.noaa.gov/data/meso-eta-hi/",YM,"/",Date, "/", sep="")
+url
+
+#Last 3 numbers decide which tile is selected from the full image. Need to determine which one to use.
+destfile <- paste("nam_218_",Date,"_0000_025.grb2")
+
+download.file(url, destfile, method = "auto", quiet = FALSE)
+#This is downloading the wrong file and I am not sure why? The file size is significantly smaller than what is described.
+
+#I am not sure how to get the .grb2 file read and in R so I can overlay it on the map.
+r <- readGDAL(destfile)
+
 ui <- fluidPage(
-  titlePanel("State College, PA"),
-    leafletOutput("mymap")
+	titlePanel("State College, PA"), 
+	leafletOutput("mymap")
 )
 
-server <- function(input, output) {
+shinyserver <- function(input, output) {
   
   output$mymap <- renderLeaflet({
     m <- leaflet() %>%
@@ -21,4 +41,4 @@ server <- function(input, output) {
   })
 }
 
-shinyApp(ui, server)
+shinyApp(ui, shinyserver)
