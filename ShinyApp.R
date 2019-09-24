0472127d3ca31cbe5ef42a0426f03e7b3e074b1a
Weiming-Hu <weiming@psu.edu>
2019-09-24 09:43:05 -0400

    meeting on Sep 24

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,10 +1,11 @@
-for (library in c('shiny', 'leaflet', 'maps','raster','stringr','rgdal')) {
-	if (requireNamespace(library)) {
-		install.packages(library)
-	} else {
-		library(library)
-	}	
-}
+# for (library in c('shiny', 'leaflet', 'maps','raster','stringr','rgdal')) {
+# 	if (requireNamespace(package = library)) {
+# 		library(library)
+# 	} else {
+# 		msg <- paste("Package not found", library)
+# 		stop(msg)
+# 	}	
+# }
 
 #Hardcoded the date here so it works. Can be changed by removing line 12 and changing 'date2' to 'Date'.
 date1 <- Sys.Date()
@@ -14,17 +15,36 @@ Date <- "20190920"
 
 YM <- substr(Date, 1, nchar(Date)-2)
 
-url <- paste("https://nomads.ncdc.noaa.gov/data/meso-eta-hi/",YM,"/",Date, "/", sep="")
-url
-
 #Last 3 numbers decide which tile is selected from the full image. Need to determine which one to use.
-destfile <- paste("nam_218_",Date,"_0000_025.grb2")
+destfile <- paste("nam_218_", Date, "_0000_025.grb2", sep = '')
 
-download.file(url, destfile, method = "auto", quiet = FALSE)
+url <- paste("https://nomads.ncdc.noaa.gov/data/meso-eta-hi/",
+             YM,"/",Date, "/", destfile, sep="")
+
+destfile <- paste0('output/', destfile)
+
+ret <- download.file(url, destfile, method = "auto", quiet = FALSE)
+
+# Check whether the download succeeded
+if (ret != 0) {
+  stop('Download failed!')
+}
+
 #This is downloading the wrong file and I am not sure why? The file size is significantly smaller than what is described.
 
+# File conversion from grb2 to NetCDF
+command <- paste(
+  'gribConverter -c grb2nc-NAM.cfg',
+  '--folder ./output/',
+  '--output ./output/output.nc')
+
+ret <- system(command)
+
 #I am not sure how to get the .grb2 file read and in R so I can overlay it on the map.
-r <- readGDAL(destfile)
+# r <- readGDAL(destfile)
+library(ncdf4)
+
+# ...
 
 ui <- fluidPage(
 	titlePanel("State College, PA"), 
